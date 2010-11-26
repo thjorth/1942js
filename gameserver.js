e7ec143fb1d189b4ac00d760e8f40b464c3311d9
jacob.andresen <jacob.andresen@gmail.com>
2010-11-26 21:20:00 +0000

    jslinting

--- gameserver.js
+++ gameserver.js
@@ -1,17 +1,16 @@
 var http = require('http'),
   io = require('socket.io'),
-  server = http.createServer(function(req, res) {
+  server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>hello world </h1>');
+    res.write('<h1>game server </h1>');
     res.end();
   });
 
 server.listen(8080);
+var io = io.listen(server), buffer = [];
 
-var io = io.listen(server) , buffer = [];
-
-io.on('connection', function(client) {
-  client.on('message', function(message) {
+io.on('connection', function (client) {
+  client.on('message', function (message) {
     var msg = { message: [client.sessionId, message] };
     client.broadcast(msg);
   });
--- queue.js
+++ queue.js
@@ -1,46 +1,48 @@
+/*global Game*/
+/*jslint onevar: false*/
 Game.queue = {
-	objs: [],
-	move: function(dt) {
-		var obj = null;
-		for (var i = 0, ix = Game.queue.objs.length; i < ix; i++) {
-			obj = Game.queue.objs[i];
-			if (obj.valid) {
-				obj.move(dt);
-				obj.updateValid();
-			}
-		}
-	},
-	draw: function() {
-		var obj = null;
-		for (var i = 0, ix = Game.queue.objs.length; i < ix; i++) {
-			obj = Game.queue.objs[i];
-			if (obj.valid) {
-				obj.draw();
-			}
-		}
-	},
-	newObject: function(type) {
-		// first see if there's an old object we could use
-		var obj = null;
-		for (var i = 0, ix = Game.queue.objs.length; i < ix; i++) {
-			obj = Game.queue.objs[i];
-			if (!obj.valid && obj.type == type) {
-				obj.reinit();
-				return obj;
-			}
-		}
-		switch (type) {
-			case "playerBullet":
-				obj = new Game.PlayerBullet();
-				break;
-			case "player" :
-				obj = new Game.Player();
-				break;
-			default:
-				obj = new Game.Object();
-				break;
-		}
-		Game.queue.objs.push(obj);
-		return obj;
-	}
-}
+  objs: [],
+  move: function (dt) {
+    var obj = null;
+    for (var i = 0, ix = Game.queue.objs.length; i < ix; i = i + 1) {
+      obj = Game.queue.objs[i];
+      if (obj.valid) {
+        obj.move(dt);
+        obj.updateValid();
+      }
+    }
+  },
+  draw: function () {
+    var obj = null;
+    for (var i = 0, ix = Game.queue.objs.length; i < ix; i = i + 1) {
+      obj = Game.queue.objs[i];
+      if (obj.valid) {
+        obj.draw();
+      }
+    }
+  },
+  newObject: function (type) {
+    // first see if there's an old object we could use
+    var obj = null;
+    for (var i = 0, ix = Game.queue.objs.length; i < ix; i = i + 1) {
+      obj = Game.queue.objs[i];
+      if (!obj.valid && obj.type === type) {
+        obj.reinit();
+        return obj;
+      }
+    }
+    switch (type) {
+    case "playerBullet":
+      obj = new Game.PlayerBullet();
+      break;
+    case "player" :
+      obj = new Game.Player();
+      break;
+    default:
+      obj = new Game.Object();
+      break;
+    }
+    Game.queue.objs.push(obj);
+    return obj;
+  }
+};
